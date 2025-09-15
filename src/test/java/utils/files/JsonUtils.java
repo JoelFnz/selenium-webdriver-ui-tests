6f1363b363550c92eadfec96b859042aa9f5515e
Joel Fernandez <joelfernandez.relg@gmail.com>
2025-09-15 16:26:54 -0300

    Add JsonUtils class for serialize/deserialize JSON files

---
+++ src/test/java/utils/files/JsonUtils.java
@@ -0,0 +1,47 @@
+package utils.files;
+
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import lombok.experimental.UtilityClass;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@UtilityClass
+public class JsonUtils {
+    private final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public <T> T deserializeJson(String filePath, Class<T> tClass) {
+        try (FileReader reader = new FileReader(filePath)) {
+            return GSON.fromJson(reader, tClass);
+        } catch (IOException | JsonSyntaxException e) {
+            handleFileException(filePath, e);
+        }
+        return null;
+    }
+
+    public void serializeJson(String filePath, Object object) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            GSON.toJson(object, writer);
+        } catch (IOException | JsonSyntaxException e) {
+            handleFileException(filePath, e);
+        }
+    }
+
+    public String getValue(String filePath, String attributeName) {
+        String value = null;
+        try (FileReader reader = new FileReader(filePath)) {
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+            value = (jsonObject.has(attributeName)) ? jsonObject.get(attributeName).getAsString() : null;
+        } catch (IOException e) {
+            handleFileException(filePath, e);
+        }
+        return value;
+    }
+
+    private void handleFileException(String filePath, Exception e) throws RuntimeException {
+        throw new RuntimeException("Could not serialize/deserialize JSON at '%s'\nCause: %s".formatted(filePath, e));
+    }
+}
